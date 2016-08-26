31b7de7088d168985c197a4231e8747a7260a190
danielarnost <danielarnost@utexaas.edu>
2016-08-26 18:54:53 -0500

    workingish

--- javascript/app.js
+++ javascript/app.js
@@ -1,26 +1,39 @@
 //alert("hello");
+$(document).ready(function(){
+//$('.q, .end').hide(q);
+
+
+//$('#timebutton') .on('click', function(event){
+//$('.q, .end').show(q);	
+
+
+
+
+//})
 
 $QuestionNum = 1;
 $radionum = 1;
 
-var count = 120;
-var counter= setInterval(timer,1000);
+//var count = 120;
+//var counter= setInterval(timer,1000);
 var correctAnswers
 var incorrectAnswers
 var unanswered
 
-function timer(){
-		count--;
-		if(count == 0){
+//$('#timer') .on('click', function(event){
 
-				clearInterval(counter);
-		}
-document.getElementById("num").innerHTML = count;
-}
+//function timer(){
+		//count--;
+		//if(count == 0){
+
+			//	clearInterval(counter);
+	//	}
+//document.getElementById("num").innerHTML = count;
+//}
+//})
 
 
-
-$(document).ready(function(){
+//$(document).ready(function(){
 
 
 
@@ -29,9 +42,24 @@ $(document).ready(function(){
 		
 		
 		
-			//$("#num").html(count);
+			$(document).ready(function() {
+                    var token;
+                    var count = 120;
+                    var counter = setInterval(minusSec, 1000);
+
+                    function minusSec() {
+                        count--;
+                        $("#num").html(count);
+                   
+                     if (count === 0) {
+
+                            clearInterval(counter);
+                        };
+                     };
+                 });
+
 
 });
+//$('.q, .end').hide(q);
 
-
-//$('.q, .end').hide() //$ is a function to select multiple classes, use syntact in Parens .hide is a function
+ //$ is a function to select multiple classes, use syntact in Parens .hide is a function
